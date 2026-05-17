f40adb93337a86b8a3639952c473f507dd907f23
Nawar Hasan <nawarhhasan@gmail.com>
2026-05-17 15:13:08 +0300

    Add solution - CodeHub

---
+++ 7-kyu/moves-in-squared-strings-i/moves-in-squared-strings-i.py
@@ -0,0 +1,7 @@
+def vert_mirror(s):    
+    return '\n'.join(x[::-1] for x in s.split('\n'))
+def hor_mirror(s):
+    return '\n'.join(x for x in s.split('\n')[::-1])   
+def oper(fct, s):
+    return fct(s) 
+print(oper(vert_mirror, "abcd\nefgh\nijkl\nmnop"))
