59ed25320939c6517f2095177680b83d5182f4d3
Nawar Hasan <nawarhhasan@gmail.com>
2026-06-17 17:10:14 +0300

    Add solution - CodeHub

---
+++ 7-kyu/rotate-for-a-max/rotate-for-a-max.py
@@ -0,0 +1,8 @@
+def max_rot(n): 
+    s = str(n)
+    max_num = n
+    for i in range(len(s)):
+        s = s[:i] + s[i+1:] + s[i]
+        max_num = max(max_num, int(s))
+    return max_num
+print(max_rot(56789))
